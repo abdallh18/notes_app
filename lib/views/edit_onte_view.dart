a5a24b1505adce566b7e9023db24868ea08d017e
abdallh <Abdallh.kut18@gmail.com>
2023-05-11 09:59:42 +0300

    create edit note view

---
+++ lib/views/edit_onte_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../widgets/edit_note_view_body.dart';
+import '../widgets/notes_view_body.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EditNotsViewBody();
+  }
+}
